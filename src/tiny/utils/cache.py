515978c01f57f7f8d088606581e58528968ead5f
Argishti <aargo707@gmail.com>
2026-01-14 20:52:57 +0300

    purge: fastapi-cache2 utils

--- src/tiny/utils/cache.py
+++ src/tiny/utils/cache.py
@@ -1,26 +1,5 @@
-"""Custom key builders for fastapi_cache2"""
-
 import json
 from typing import Any, Optional
-
-
-def user_key_builder(
-    func,
-    namespace: str = "",
-    request=None,
-    response=None,
-    *args,
-    **kwargs,
-):
-    user_id = kwargs.get("user_id")
-
-    if user_id is None and args:
-        user_id = args[0]
-
-    if user_id is None and request is not None:
-        user_id = request.path_params.get("user_id")
-
-    return f"{namespace}:{user_id}"
 
 
 def serialize(value: Any) -> str:
